fd5a643523b05cdd0c05afb356f4444cd80fed08
TimZhang <zewenzhang@gmail.com>
2016-09-09 14:48:34 +0800

    Scoring

--- douban.js
+++ douban.js
@@ -1,21 +1,72 @@
 var searchUrl = 'http://api.douban.com/v2/movie/search?q='
 
 var searchMovie = function(name) {
-	var movieInStore, url;
+  var movieInStore, url;
 
-	movieInStore = getStorage(name);
-	if (movieInStore) return Promise.resolve(movieInStore);
+  movieInStore = getStorage(name);
+  if (movieInStore) return Promise.resolve(movieInStore);
 
-	url = searchUrl + encodeURI(name);
-	return fetch(url).then(res => res.json()).then(function(data) {
-		if (data.count === 0) return Promise.reject();
 
-		return setStorage(name, data.subjects[0]);
-	});
+  url = searchUrl + encodeURI(name);
+  return fetch(url).then(res => res.json()).then(function(data) {
+    if (data.count === 0) return Promise.reject();
+    return setStorage(name, scoring(data.subjects));
+  });
+}
+
+var scoring = function(movies) {
+  var titles = getTitles()
+    , cast = getCast()
+    , genres = getGenres()
+    , maxScore = 0
+    , chosen = null
+    , movieCount = movies.length
+
+
+  movies.forEach((movie, idx) => {
+    movie.score = (movieCount - idx) / (movieCount * 10)
+
+    if (titles.includes(movie.title) || titles.includes(movie.original_title)) {
+      movie.score += 11
+    }
+
+    movie.casts.forEach(c => {
+      if (cast.includes(c.name)) movie.score += 10
+    })
+
+    movie.genres.forEach(g => {
+      if (genres.includes(g)) movie.score += 5
+    })
+
+    if (movie.score > maxScore) {
+      maxScore = movie.score
+      chosen = movie
+    }
+  })
+
+  return chosen
+}
+
+var getCast = function() {
+  return queryText('.info li:nth-child(3) a')
+}
+
+var getGenres = function() {
+  return queryText('.info li:nth-child(2) a')
+}
+
+var getTitles = function() {
+  return document.querySelector('.pic img').getAttribute('alt').split('/')
+}
+
+var queryText = function(selector) {
+  var results = []
+  document.querySelectorAll(selector).forEach(a => results.push(a.textContent))
+  return results
 }
 
 var isSinglePage = function() {
-	return /\/Html\/\w+\.html/.test(location.pathname);
+  return /\/Html\/\w+\.html/.test(location.pathname);
 }
 
 var setStorage = function(name, value) {
@@ -35,15 +86,14 @@ var getStorage = function(name) {
 }
 
 var processSinglePage = function() {
-		var name      = document.querySelector('.pic img').getAttribute('alt')
-		  , names     = name.split('/')
-		  , firstName = names[0]
-		  , result
+    var names     = getTitles()
+      , firstName = names[0]
+      , result
 
-		searchMovie(firstName).then(function(subject) {
-			var rating  = subject.rating.average
-			  , poster  = subject.images.large
-			  , url     = subject.alt
+    searchMovie(firstName).then(function(subject) {
+      var rating  = subject.rating.average
+        , poster  = subject.images.large
+        , url     = subject.alt
         , text    = ''
 
       if (rating)
@@ -51,15 +101,15 @@ var processSinglePage = function() {
       else
         text = '豆瓣评价人数不足';
 
-			result = '<a style="color: #072;" href="' + url + '">' + text + '</a>';
-			render(result);
-		}).catch(function() {
-			render('豆瓣没找到结果');
-		});
+      result = '<a style="color: #072;" href="' + url + '">' + text + '</a>';
+      render(result);
+    }).catch(function() {
+      render('豆瓣没找到结果');
+    });
 }
 
 var render = function(content) {
-	var style = 'font-size: 16px; margin-left: 5px; padding-left: 9px; border-left: 1px solid #8E8E8E'
+  var style = 'font-size: 16px; margin-left: 5px; padding-left: 9px; border-left: 1px solid #8E8E8E'
     , doubanScoreEl = document.createElement('span')
 
   doubanScoreEl.style.cssText = style;
@@ -72,9 +122,9 @@ var render = function(content) {
 }
 
 switch(true) {
-	case /\/Html\/\w+\.html/.test(location.pathname):
-		processSinglePage();
-		break;
+  case /\/Html\/\w+\.html/.test(location.pathname):
+    processSinglePage();
+    break;
 }
 
 
